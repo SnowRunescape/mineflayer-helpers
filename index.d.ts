d54c2dcd606be47153bcc3ba13eaffda24660762
Bruno Caitano <snowrunescape@snowdev.com.br>
2023-09-21 21:31:46 -0400

    feat: disable generate index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,12 @@
 import { Bot } from "mineflayer";
+import { Item } from "prismarine-item";
 
 declare module 'mineflayer-utils';
 
 export function getDirection(bot: Bot): string;
+export function getHeldItemId(bot: Bot): number;
+export function getHeldItemDurability(bot: Bot): number;
+
+export function tryGetItemIdByNome(bot: Bot, value: number|string): number;
+export function getItemBySlotId(bot: Bot): number;
+export function getHeldItemDurability(slotId: number, items: Item[]): Item | undefined;
